ce59562cb5d53ff677f9f3a1ecc271c30a685130
Elio-yang <2925539854@qq.com>
2020-11-02 20:52:44 +0800

    refeactor & week2_Q1

---
+++ NOI/id_1696_polish.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+
+#include <stack>
+
+using namespace std;
+stack<double> value;
+
+int main()
+{       
+        string s1;
+        bool f=false;
+        string polish_str[100];
+        int n=0;
+        while(cin>>polish_str[n++]){}
+        n--;
+        for(int i=n-1;i>=0;i--){
+                if(isdigit(polish_str[i][0])){
+                        value.push(atof(polish_str[i].c_str()));
+                }
+                else{
+                        if(value.size()<2){f=true;break;}
+                        /*注意错误处理*/
+                        char c=polish_str[i][0];
+
+                        double op1=value.top();
+                        value.pop();
+                        double op2=value.top();
+                        value.pop();
+
+                        double ans=0.0;
+                        switch(c){
+                                case '+':{
+                                        ans=op1+op2;
+                                        value.push(ans);
+                                        break;
+                                }
+                                case '-':{
+                                        ans=op1-op2;
+                                        value.push(ans);
+                                        break;
+
+                                }
+                                case '*':{
+                                        ans=op1*op2;
+                                        value.push(ans);
+                                        break;
+
+                                }
+                                case '/':{
+                                        if(op2==0){
+                                                f=true;
+                                                break;
+                                        }
+                                        ans=op1/op2;
+                                        value.push(ans);
+                                        break;
+                                }
+                        }
+                }
+        }
+       
+       if(value.size()!=1){f=true;}
+       /*错误处理*/
+       if(f){
+               printf("ERROR\n");
+       }else{
+                double l_v=value.top();
+                printf("%f\n",l_v);
+       }
+       
+       system("pause");
+        return 0;
+
+}
