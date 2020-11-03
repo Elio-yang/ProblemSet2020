a29654fd8b18a719644c87e25c21ce0c8ceadba2
Elio-yang <2925539854@qq.com>
2020-11-03 23:53:20 +0800

    other ways

--- hduOJ/11_3/id_2044.cpp
+++ hduOJ/11_3/id_2044.cpp
@@ -1,12 +1,28 @@
 #include <cstdio>
-
+#include <cstring>
+/*f[i]=f[i-1]+f[i-2]*/
+long long int mem[55];
+long long int fibo(int m);
 int main()
 {
         int n;
-        scanf("%d",&n);
-        
-
-
-
+        while(~scanf("%d",&n)){
+                while(n--){
+                        int a,b;
+                        scanf("%d %d",&a,&b);
+                        long long int n=fibo(b-a);
+                        printf("%lld\n",n);
+                }
+        }
         return 0;
+}
+long long int fibo(int m)
+{
+        if(m<=1){
+                return 1;
+        }
+        if(mem[m]==0){
+                mem[m]=fibo(m-1)+fibo(m-2);
+        }
+        return mem[m];
 }
