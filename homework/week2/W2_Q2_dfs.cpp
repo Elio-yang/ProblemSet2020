51c1c96edbe1d0e50fd53977e673b8784595053c
Elio-yang <2925539854@qq.com>
2020-11-15 11:50:46 +0800

    backtrack

--- homework/week2/W2_Q2_dfs.cpp
+++ homework/week2/W2_Q2_dfs.cpp
@@ -1,46 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 /*回溯法*/
 #define maxn 100000
-void bfs(char *str, int n, int len);
+
+void dfs(char *str, int len);
+
 int main()
 {
-    int N;
-    char ch='a';
-    scanf("%d", &N);
-    char* str = (char*)malloc(sizeof(char)*N);
-    for (int i = 0; i < N; i++){
-        str[i] = ch++;
-    }    
-    int k = 0;
-    bfs(str, k, N);
-    system("pause");
-    return 0;
-}
-void dfs(char *str, int n, int len)
-{
-    int i, cnt = 0;
-    char ans[maxn];
-    ans[cnt] = 'a';
-    printf("{}\n");
-    do{
-        printf("{");
-        for (i = 0; i < cnt; i++)
-        {
-            printf("%c,", ans[i]);
-        }
-        printf("%c",ans[i]);
-        printf("}\n");
+        int N;
+        scanf("%d", &N);
+        char *str = (char *) malloc(sizeof(char) * N);
 
-        if (ans[cnt] < 'a' + len - 1){
-            ans[cnt + 1] = ans[cnt] + 1;
-            ++cnt;
+        char ch = 'a';
+        for (int i = 0; i < N; i++) {
+                str[i] = ch++;
         }
-        else if (cnt){
-            ans[--cnt] += 1;
-        }
-        else{
-            break;
-        }
-    }while(1);
+        dfs(str, N);
+        return 0;
+}
+
+void dfs(char *str, int len)
+{
+        int i, cnt = 0;
+        char ans[maxn];
+        ans[cnt] = 'a';
+        printf("{}\n");
+        do {
+                printf("{");
+                for (i = 0; i < cnt; i++) {
+                        printf("%c,", ans[i]);
+                }
+                printf("%c", ans[i]);/*处理逗号*/
+                printf("}\n");
+
+                if (ans[cnt] < 'a' + len - 1) {
+                        ans[cnt + 1] = ans[cnt] + 1;
+                        ++cnt;
+                } else if (cnt) {
+                        ans[--cnt] += 1;
+                } else {
+                        break;
+                }
+        } while (1);
 }
