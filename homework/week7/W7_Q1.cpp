6889e471cc218643e8f5f8f80e5d6cfa846ebb12
Elio-yang <2925539854@qq.com>
2020-12-09 01:23:03 +0800

    week 7

--- homework/week7/W7_Q1.cpp
+++ homework/week7/W7_Q1.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 #define maxn  300
 const int inf=0x3f3f3f3f;
+
 template<typename T>
 class queue{
         static constexpr int Empty=-1;
@@ -33,11 +34,12 @@ public:
         bool empty(){
                 return size==0;
         }
-        ~queue<T>(){
-                delete []arr;
+        void clear(){
+                Front=1;
+                Rear=0;
+                size=0;
         }
 };
-
 template<typename T>
 class stack{
 private:
@@ -80,6 +82,10 @@ public:
         int size() const{
                 return _size;
         }
+        void clear(){
+                Top=-1;
+                _size=0;
+        }
 };
 
 struct Pair {
@@ -112,7 +118,6 @@ stack<int> topo_order(maxn);
 queue<int> q;
 Pair key_path[300];
 
-
 class AOE{
 public:
         int n;
@@ -121,9 +126,11 @@ public:
         int G[maxn][maxn];
         int ve[maxn];
         int vl[maxn];
+        bool flag= true;
 
         AOE(int N,int E):n(N),e(E),cnt(0){}
         bool topological_sort(){
+                int num=0;
                 for (int i = 1; i <= n; i++) {
                         if (deg[i].in==0){
                                 q.push(i);
@@ -133,6 +140,7 @@ public:
                         int v=q.front();
                         q.pop();
                         topo_order.push(v);
+
                         for (int i = 1; i <= n; i++) {
                                 if(G[v][i]!=inf){
                                         deg[i].minus_in();
@@ -144,20 +152,34 @@ public:
                                         }
                                 }
                         }
+                        num++;
                 }
-                if (topo_order.size()==n){return true;}
-                else {return false;}
+                if (num!=n){
+                        printf("unworkable project\n");
+                        flag= false;
+                        return false;
+                }
+                else{
+
+                        return true;
+                }
         }
         void CRITICAL_PATH(){
                 memset(ve,0,sizeof(ve));
                 if (!topological_sort()){
-                        printf("unworkable project\n");
                         return;
                 }
-                /*求vl数组*/
-                for (int i = 1; i <= n; i++) {
-                        vl[i]=ve[n];
+                /*不确定n是最后一个汇点，找到最大的ve 才能更新vl*/
+                int max=-1;
+                for (int i = 1; i <=n ; i++) {
+                        if (ve[i]>max){
+                                max=ve[i];
+                        }
                 }
+                for (int i = 1; i <= n; i++) {
+                        vl[i]=max;
+                }
+                /*求vl数组*/
                 while (!topo_order.empty()){
                         int u=topo_order.top();
                         topo_order.pop();
@@ -180,29 +202,57 @@ public:
                                 }
                         }
                 }
-                printf("%d\n",ve[n]);
+                printf("%d\n",max);
         }
 };
 
 int main()
 {
         int n,e;
-        scanf("%d %d",&n,&e);
-        AOE aoe(n,e);
-        memset(aoe.G,inf,sizeof(aoe.G));
-        for (int i = 0; i < e; i++) {
-                int u,v,w;
-                scanf("%d %d %d",&u,&v,&w);
-                //DAG图
-                //u-->v
-                aoe.G[u][v]=w;
-                deg[u].add_out();
-                deg[v].add_in();
-        }
-        aoe.CRITICAL_PATH();
-        qsort(key_path,aoe.cnt,sizeof(key_path[0]),cmp);
-        for (int i = 0; i < aoe.cnt; i++) {
-                printf("%d->%d\n",key_path[i].f,key_path[i].s);
+        while (scanf("%d %d",&n,&e)!=EOF){
+                AOE aoe(n,e);
+                memset(aoe.G,inf,sizeof(aoe.G));
+                for (int i = 0; i < e; i++) {
+                        int u,v,w;
+                        scanf("%d %d %d",&u,&v,&w);
+                        aoe.G[u][v]=w;
+                        deg[u].add_out();
+                        deg[v].add_in();
+                }
+                aoe.CRITICAL_PATH();
+                if (aoe.flag) {
+                        qsort(key_path, aoe.cnt, sizeof(key_path[0]), cmp);
+                        for (int i = 0; i < aoe.cnt; i++) {
+                                printf("%d->%d\n", key_path[i].f, key_path[i].s);
+                        }
+                }
+                topo_order.clear();
+                q.clear();
+                memset(deg,0,sizeof(deg));
+                memset(key_path,0,sizeof(key_path));
+                /*最后要清除*/
         }
         return 0;
 }
+
+/*
+4 3
+1 2 3
+2 3 4
+3 4 5
+12
+1->2
+2->3
+3->4
+
+3 3
+1 2 3
+2 3 1
+3 1 2
+unworkable project
+
+4 3
+1 2 3
+2 3 4
+3 4 5
+ */
